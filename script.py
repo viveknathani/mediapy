6acf7115d91214115ed098b00952874ec1b4bdee
viveknathani <viveknathani2402@gmail.com>
2020-03-26 04:52:07 +0530

    added stats

--- script.py
+++ script.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import pytube
+import speedtest
 import subprocess
 from selenium import webdriver
 from progress.bar import FillingSquaresBar
@@ -13,9 +14,31 @@ identify_content=argument_list[1] # s or p
 file_type=argument_list[2] # m or v
 url=argument_list[3]
 
+def get_speed():
+    print("Getting connection speed : ", end=" ")
+    st=speedtest.Speedtest()
+    speed=st.download()*1.25*pow(10, -7)
+    speed_string=str(round(speed, 2))+"MB/s"
+    print(speed_string)
+
+def get_size(s):
+    video_size=(float(s)/1048576)
+    size_string=str(round(video_size, 2))+"MB"
+    return size_string
+
+def get_time(s):
+    time_value=divmod(s, 60)
+    time_string=str(time_value[0])+"m "+str(time_value[1])+"s"
+    return time_string
+
 def download_video(link):
     yt=pytube.YouTube(link)
-    bar=FillingSquaresBar("Downloading Video : ", suffix="%(percent)d%%")
+    stream=yt.streams.first()
+    video_length=get_time(yt.length)
+    video_size=get_size(stream.filesize)
+    print("Downloading \""+yt.title+"\" Length : "+video_length)
+    print("\tFile Size : "+video_size)
+    bar=FillingSquaresBar("Progress : ", suffix="%(percent)d%%")
     for i in range(100):
         stream=yt.streams.first()
         stream.download(SAVE_PATH)
@@ -27,12 +50,10 @@ def download_audio(link):
     bar=FillingSquaresBar("Downloading Audio : ", suffix="%(percent)d%%")
     for i in range(100):
         stream=yt.streams.filter(only_audio=True).first()
-        _filename=yt.title.replace(" ", "_")
-        _filename=_filename.replace(".", "_")
-        _filename=_filename.replace("$", "_")
-        _filename=_filename.replace("@", "_")
-        _filename=_filename.replace("(", "_")
-        _filename=_filename.replace(")", "_")
+        bad_chars=[";", ":", "!", "*", ' ', "$", "@", "(", ")", "[", "]", "|", ".", "\"", "\'", ","]
+        _filename=yt.title
+        for i in bad_chars:
+            _filename=_filename.replace(i, "_")
         mp4_name="download/%s.mp4"%_filename
         mp3_name="download/%s.mp3"%_filename
         stream.download(SAVE_PATH, _filename)
@@ -43,6 +64,8 @@ def download_audio(link):
     subprocess.call(ffmpeg, shell=True)
     os.remove(mp4_name)
     bar.finish()
+
+get_speed()
 
 if(identify_content=="s" and file_type=="v"):
     download_video(url)
